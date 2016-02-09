00f37877b067f5698c2f664fabe6a8ad60f22e5e
MarvinHawkins <m.louis.hawkins@gmail.com>
2016-02-08 21:11:38 -0600

    First Checkin

---
+++ Assets/buildSpot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buildSpot : MonoBehaviour {
+
+    public GameObject buildPanel; //t he build pqanel
+    public GameObject buildPrefab;
+
+
+    // Use this for initialization
+    void Start () {
+        buildPanel = Instantiate(buildPrefab) as GameObject;
+        buildPanel.SetActive(false);
+
+    }
+
+    void OnMouseUp()
+    {
+        buildPanel.SetActive(true);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+	
+	}
+}
